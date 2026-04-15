7b49b63f4d22c45f034c332709a52b277a2ffe6d
Manish Lakkavatri <kirtithatikonda309@gmail.com>
2026-04-15 11:47:30 +0530

    first commit

---
+++ src/main/java/com/example/blogmanagementsystem/entity/Post.java
@@ -0,0 +1,30 @@
+package com.example.blogmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
