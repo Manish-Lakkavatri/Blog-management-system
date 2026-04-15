365ccf01e9d41ffd366b29377d3457efdec14dd0
Manish Lakkavatri <manish.lakkavatri783@gmail.com>
2026-04-15 12:03:56 +0530

    Added JPA Repositories

---
+++ src/main/java/com/example/blogmanagementsystem/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.example.blogmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 1000)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/com/example/blogmanagementsystem/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.blogmanagementsystem.repository;
+
+import com.example.blogmanagementsystem.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
